24c3bb60e08bd59532fc1dc93f73ec467fc80fce
ipsi16 <ipsita16@hotmail.com>
2015-07-22 10:01:56 +0530

    final code

--- sowpods.cpp
+++ sowpods.cpp
@@ -3,15 +3,14 @@
 #include<cstring>
 #include<map>
 using namespace std;
-int main()
-{
+int main(){
 	char primes[]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
 	unsigned long p=1;
 	int index;
-	int ctr = 0;
 	string line;
 	map<unsigned long int, string> m;
     ifstream myfile ("sowpods_data.txt");
+    
     if (myfile.is_open()){
     	while ( getline (myfile,line) ){
     		p = 1, index = 0;
@@ -19,13 +18,18 @@ int main()
 	    	while(index < size){
 				p *= primes[line[index++] - 97];
 			}
-			m[p]= m[p] + line + " ";
-			//cout<<m[p]<<" "<<p<<endl;
-			ctr++; 
+			if(m.find(p) == m.end()){
+				m[p]=line;
+			}
+			else {
+			m[p]= m[p] + " " + line;
+		    } 
     	}
-    	    	
-    	for(map<unsigned long int, string>::iterator it = m.begin(); it != m.end(); it++){
-    		cout<<it->second<<endl;
+    	ofstream myfile ("sowpods_output.txt");
+    	for(map<unsigned long int, string>::iterator it = m.begin(); it != m.end() ; it++){
+    		if(it->second.find(" ",0) != -1){
+    		myfile<<it->second<<endl;
+			}
 		}
     myfile.close();
     }
