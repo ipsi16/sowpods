9e64ab76a2dbf6bd13f9ec2985591ef8faf4bea3
ipsi16 <ipsita16@hotmail.com>
2015-07-22 10:33:17 +0530

    Final readable code

--- sowpods.cpp
+++ sowpods.cpp
@@ -3,42 +3,52 @@
 #include<cstring>
 #include<map>
 using namespace std;
-int main(){
-	char primes[]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
-	unsigned long p=1;
+
+map<unsigned long int, string>  createAnagramMap(ifstream& myfile){
+	char primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+	unsigned long product = 1;
 	int index;
-	string line;
-	map<unsigned long int, string> m;
-    ifstream myfile ("sowpods_data.txt");
+	string word;
+	map<unsigned long int, string> anagram_map;    
     
     if (myfile.is_open()){
-    	while ( getline (myfile,line) ){
-    		p = 1, index = 0;
-    		int size = line.size();
-	    	while(index < size){
-				p *= primes[line[index++] - 97];
+    	while (getline (myfile,word)){
+    		product = 1, index = 0;
+    		int size = word.size();
+	    	while (index < size){
+				product *= primes[word[index++] - 'a'];
 			}
-			if(m.find(p) == m.end()){
-				m[p]=line;
+			if (anagram_map.find(product) == anagram_map.end()){
+				anagram_map[product] = word;
 			}
 			else {
-			m[p]= m[p] + " " + line;
+			anagram_map[product] = anagram_map[product] + " " + word;
 		    } 
     	}
-    	ofstream myfile ("sowpods_output.txt");
-    	for(map<unsigned long int, string>::iterator it = m.begin(); it != m.end() ; it++){
-    		if(it->second.find(" ",0) != -1){
-    		myfile<<it->second<<endl;
-			}
-		}
+    	
     myfile.close();
+    return anagram_map;
     }
 
     else {
 	 cout << "Unable to open file";
     }
+}
+void printAnagrams(map<unsigned long int, string> anagram_map)
+{
+	ofstream myfile("sowpods_output.txt");
+    	for (map<unsigned long int, string>::iterator it = anagram_map.begin(); it != anagram_map.end() ; it++){
+    		if (it->second.find(" ",0) != -1){
+    		myfile<<it->second<<endl;
+			}
+		}
+}
+
+int main(){
+	
+	ifstream myfile("sowpods_data.txt");
+	map<unsigned long int, string> m = createAnagramMap(myfile);
+	printAnagrams(m);
     getchar();
     return 0;
-	
-	
 }
